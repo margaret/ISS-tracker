6699d112dc66cfdd5afa117a8624f91a305603db
Margaret Sy <margaretsy1016@gmail.com>
2015-12-12 18:41:05 -0800

    try getting ISS location

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,9 @@
 function getIssLocation() {
-	console.log("Hi.")
+	$.getJson('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
+		var lat = data['iss-position']['latitude'];
+		var lon = data['iss-position']['longitude'];
+		console.log(lat, lon)
+	})
 }
 
 var main = function() {
