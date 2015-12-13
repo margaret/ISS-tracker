349398c57c575e7a2d29e3a660bacff287d2c117
Margaret Sy <margaretsy1016@gmail.com>
2015-12-12 18:26:08 -0800

    move initMap to index

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,9 @@
-function initMap(latitude = -25.363, longitutde = 131.044) {
-  var myLatLng = {lat: latitude, lng: longitutde};
-
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    scrollwheel: false,
-    zoom: 4
-  });
-
-  // Create a marker and set its position.
-  var marker = new google.maps.Marker({
-    map: map,
-    position: myLatLng,
-    title: 'Hello World!'
-  });
-}
-
 function getIssLocation() {
 	console.log("Hi.")
 }
 
 var main = function() {
 	getIssLocation();
-	initMap();
 }
 
 $(document).ready(main);
