fcdea32e08d1e5369c66e2e8e967b8ce0440c336
Margaret Sy <margaretsy1016@gmail.com>
2015-12-16 19:33:09 -0800

    try getting flyover times on press enter

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,14 @@
 
 
 var main = function() {
-	console.log("Hi there.")
+	console.log("Hi there.");
+	document.getElementById('flyoverAddress').onkeypress = function(e) {
+		var event = e || window.event;
+		var charCode = event.which || event.keyCode;
+		if (charCode == '13') {
+			geocodeLocation();
+		}
+	}
 }
 
 $(document).ready(main);
