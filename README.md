44dcb5122e7b173c039c9eac855ef967080bb1df
Margaret Sy <margaretsy1016@gmail.com>
2016-01-26 01:02:33 -0800

    hackathon-quality generation of google calendar url (almost working)

--- README.md
+++ README.md
@@ -19,3 +19,10 @@ Basic implementation of ISS tracking using Google Maps Javascript API and [Open 
 ### Weather
 * find a free weather API and add a column for weather
 
+https://www.google.com/calendar/render?action=TEMPLATE&text=ISS+Flyover&dates=20160127T224000Z/20160127T224500Z&details=International+Space+Station+flyover!+Via+http://margaretsy.com/ISS-tracker&location=Philadelphia,+PA&sf=true&ctz=America/New_York&output=xml
+
+bad output
+https://www.google.com/calendar/render?action=TEMPLATE&text=ISS+Flyover&details=International+Space+Station+flyover!+Via+http://margaretsy.com/ISS-tracker&dates=20160126T82200Z/20160126T82700Z&location=Your+Mom&ctz=America/New_York&output=xml
+
+
+http://www.google.com/calendar/event?action=TEMPLATE&dates=20160128T024500Z%2f20160128T050000Z&sprop=website%3ahttp%3a%2f%2fwww.meetup.com%2fWomen-Who-Code-East-Bay%2fevents%2f228235143%2f&text=Code+Study+Meetup+in+Berkeley+%28right+next+to+the+downtown+BART%29&location=NextSpace+Berkeley+-+2081+Center+Street+-+Berkeley%2C+CA&sprop=name:Women+Who+Code+East+Bay&details=For+full+details%2C+including+the+address%2C+and+to+RSVP+see%3A%0Ahttp%3A%2F%2Fwww.meetup.com%2FWomen-Who-Code-East-Bay%2Fevents%2F228235143%2F%0AWomen+Who+Code+East+Bay%0ANextSpace+Berkeley%2C+a+beautiful+co-working+space+right+next+to+the+Berkeley+downtown+BART%2C+has+graciously+offered+to+host+Women+Who+Code+East+Bay%27s...
--- static/js/app.js
+++ static/js/app.js
@@ -84,11 +84,13 @@ function getPassTimes(lat, lon, addr, formattedAddr) {
 	});
 }
 
+
 function displayPassTimes(data, formattedAddr) {
   console.log("PASS TIMES DATA")
   console.log(data)
 	var flyoverList = document.getElementById("passTimes");
 	flyoverList.innerHTML = '';
+  flyoverData = []; 
 	$('#passTimes').append('<tr><th>Flyover times for ' + formattedAddr + '</th><th>Duration</th></tr>');
 	var lat = data['request']['latitude'];
 	var lon = data['request']['longitude'];
@@ -109,6 +111,32 @@ function displayPassTimes(data, formattedAddr) {
     });
 }
 
+function calendarLink(startTime, durationMinutes, durationSeconds, tz, humanAddr) {
+  // Well yes ideally we would use the Google Calendar Javascript API
+  // But things aren't always ideal
+  // Are they
+  
+  // Why doesn't built-in URI encoding handle punctuation
+
+  // startTime is a moment-timezone object
+  // calendar time should be in UTC
+  var beginTime = startTime.format("YYYYMMDD") + "T" + startTime.format() + "Z"
+  var urlBase = "https://www.google.com/calendar/render?action=TEMPLATE&text=ISS+Flyover&details=International+Space+Station+flyover!+Via+http://margaretsy.com/ISS-tracker&output=xml" 
+  var dates = makeEventDates(beginTime, durationMinutes, durationSeconds);
+  var location = "&location=" + humanAddr.split(' ').join('+');
+  var timezone = "&ctz=" + tz;
+  return urlBase + dates + location + timezone;
+}
+
+function makeEventDates(start, minutes, seconds) {
+  // prob could separate out the construction and just pass in the date/moment
+  // I'm tired of this
+  var begin = start.utc().year() + padString(start.utc().month()+1, 2, '0', 'left') + start.utc().date() + "T" + padString(tart.utc().hour(), 2, '0', 'left') + padString(start.utc().minute(), 2, '0', 'left') + '00Z';
+  var endMoment = start.add(parseInt(minutes), 'minutes').add(parseInt(seconds), 'seconds');
+  var end = endMoment.utc().year() + padString(endMoment.utc().month()+1, 2, '0', 'left') + endMoment.utc().date() + "T" + padString(endMoment.utc().hour(), 2, '0', 'left') + padString(endMoment.utc().minute(), 2, '0', 'left') + '00Z';
+  return "&dates=" + begin + "/" + end
+}
+
 function constructTableRow(lat, lon, date, category, min, sec) {
   // insert the time and duration of a flyover with the correct timezone 
   // and color coding for time of day
@@ -117,17 +145,18 @@ function constructTableRow(lat, lon, date, category, min, sec) {
   // terms of callbacks
   var rowStart = '<tr class=' + category + '><td>';
   var rowEnd = '</td><td>' + min + ' m ' + sec + ' s</td></tr>';
-  var utc = date.getTime() / 1000
+  var utc = date.getTime();
   console.log("getting timezone info");
   // I am aware this is bad practice. If I wasn't hosting on static pages I'd read it in from a non-public file. 
   $.getJSON("http://api.geonames.org/timezoneJSON?lat=" + lat + "&lng=" + lon + "&username=" + author, function(data) {
     console.log(data)
     if (!('status' in data)) {
       var tz = data['timezoneId'];
-      var queryMoment = moment.tz(utc*1000, tz)
+      var queryMoment = moment.tz(utc, tz)
       var correctedDate = queryMoment.format("ddd, MMM Do YYYY, hh:mm:ss a");
       console.log(correctedDate);
-      $('#passTimes').append(rowStart + correctedDate + rowEnd);
+      var dateWithLink = "<a href='" + calendarLink(queryMoment, min, sec, tz, "Below the ISS") + "'>" + correctedDate + "</a>";
+      $('#passTimes').append(rowStart + dateWithLink + rowEnd);
     } else {
       $('#passTimes').append(rowStart + "Sorry, this feature is broken right now :(" + rowEnd);
     }
